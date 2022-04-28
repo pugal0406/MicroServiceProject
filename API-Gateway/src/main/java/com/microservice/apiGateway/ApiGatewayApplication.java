85f60a9241205046792fb187cd691fd521ef1590
Vijay <vijay.arunachalam1437@gmail.com>
2022-04-28 18:51:17 +0530

    Commited

--- API-Gateway/src/main/java/com/microservice/apiGateway/ApiGatewayApplication.java
+++ API-Gateway/src/main/java/com/microservice/apiGateway/ApiGatewayApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
