b6f24c9048441367103607f7c52fb9b39c179acf
pugal <pugaldev21@gmail.com>
2022-05-09 14:51:48 +0530

    pugal commited in 09-05

--- Hystrix-DashBoard/src/main/java/com/microservice/hystrix/HystrixDashBoardApplication.java
+++ Hystrix-DashBoard/src/main/java/com/microservice/hystrix/HystrixDashBoardApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
